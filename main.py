01cf9316a673b782fe8dd10e32a48d74c3becd2e
3jcn <31360292+3jcn@users.noreply.github.com>
2021-06-14 11:46:48 -0700

    Update main.py

--- main.py
+++ main.py
@@ -53,6 +53,7 @@ with header:
 with dataset:
 	st.header('La Jolla house price dataset Feb 2021')
 	st.text('Data was collected from online sites like Zillow, Redfin, Realtor, etc.')
+	st.write(data.shape)
 	data = get_data('LaJolla-02-2021.csv')
 
 	# PREPROCESSING DATA:
